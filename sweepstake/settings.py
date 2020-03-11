42d6922cd2807333a94045eb9f5c4fc8ea664ce5
Marlon Leite <marlonleite@gmail.com>
2020-03-11 14:54:05 -0300

    Add environments variable to application

--- sweepstake/settings.py
+++ sweepstake/settings.py
@@ -13,20 +13,23 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from decouple import config
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '27agekgqgfv1=)ue39xn*^(fe-06sxl%9k&-aacl=yt%1^aflp'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
 
-# Application definition
+
+# Application definition2
 
 INSTALLED_APPS = [
     'django.contrib.admin',
