957f874487a7bc2f58839f21eac7dd18c1ef83a1
Marlon Leite <marlonleite@gmail.com>
2020-03-11 14:54:19 -0300

    Update route urls

--- sweepstake/urls.py
+++ sweepstake/urls.py
@@ -2,7 +2,11 @@
 
 """
 from django.urls import path, include
+from rest_framework import routers
+
+router = routers.DefaultRouter()
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('api/', include('raffles.urls')),
 ]
